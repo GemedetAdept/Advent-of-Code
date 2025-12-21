c787bed34b940037c4ca4d276184c6f73580039b
GemedetAdept <vivian.zitek@gmail.com>
2025-12-21 16:48:32 -0700

    Add function to extract instructions from file to module

--- day_1/day_1.f90
+++ day_1/day_1.f90
@@ -1,31 +1,27 @@
 PROGRAM day_1
+    USE mod_functions, ONLY : extract_instructions
     IMPLICIT NONE
 
     INTEGER :: i
 
     CHARACTER(LEN=*), PARAMETER :: file = "day_1_practice_input.txt"
-    INTEGER, PARAMETER :: file_length = 10
-    INTEGER, PARAMETER :: instruction_length = 4
-    INTEGER, PARAMETER :: direction_length = 1
+    INTEGER, PARAMETER :: file_len = 10
+    INTEGER, PARAMETER :: instruction_len = 4
+    INTEGER, PARAMETER :: direction_len = 1
 
-    CHARACTER(LEN=instruction_length), DIMENSION(file_length) :: instructions
-    CHARACTER(LEN=direction_length), DIMENSION(file_length) :: directions
-    INTEGER, DIMENSION(file_length) :: magnitudes
-    CHARACTER(LEN=instruction_length) :: instruction
+    CHARACTER(LEN=instruction_len), DIMENSION(file_len) :: instructions
+    CHARACTER(LEN=direction_len), DIMENSION(file_len) :: directions
+    INTEGER, DIMENSION(file_len) :: magnitudes
+    CHARACTER(LEN=instruction_len) :: instruction
 
+    instructions = extract_instructions(file, file_len, instruction_len)
 
-    OPEN(1, FILE=file, STATUS="OLD", ACTION="READ")
-        DO i=1, file_length
-            READ(1,*) instructions(i)
-        END DO
-    CLOSE(1)
-
-    DO i=1, file_length
+    DO i=1, file_len
         instruction = instructions(i)
         directions(i) = instruction(1:1)
     END DO
 
-    DO i=1, file_length
+    DO i=1, file_len
         instruction = instructions(i)(2:)
         READ(instruction,"(I3)") magnitudes(i)
     END DO
--- day_1/mod_functions.f90
+++ day_1/mod_functions.f90
@@ -1,5 +1,25 @@
 MODULE mod_functions
-    IMPLICIT NONE
+IMPLICIT NONE
     PRIVATE
-    
+    PUBLIC extract_instructions
+
+CONTAINS
+
+    FUNCTION extract_instructions(in_file, in_file_len, in_instruction_len) result(out_instructions)
+        IMPLICIT NONE
+        INTEGER :: i
+
+        CHARACTER(LEN=*) :: in_file
+        INTEGER :: in_file_len
+        INTEGER :: in_instruction_len
+        CHARACTER(LEN=in_instruction_len), DIMENSION(in_file_len) :: out_instructions
+
+    OPEN(1, FILE=in_file, STATUS="OLD", ACTION="READ")
+        DO i=1, in_file_len
+            READ(1,*) out_instructions(i)
+        END DO
+    CLOSE(1)
+
+    END FUNCTION extract_instructions
+
 END MODULE
