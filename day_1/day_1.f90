7dfdde66b46be80f7b13684755acd004191af01f
GemedetAdept <vivian.zitek@gmail.com>
2025-12-21 10:59:16 -0700

    Remove module because it was becoming more trouble than it was worth for a fun exercise

--- day_1/day_1.f90
+++ day_1/day_1.f90
@@ -1,7 +1,24 @@
 PROGRAM day_1
-    USE instructions_module
     IMPLICIT NONE
 
-    TYPE(Instructions) :: new_instructions
+    INTEGER :: i
+
+    INTEGER, PARAMETER :: file_length = 10
+    INTEGER, PARAMETER :: instruction_length = 4
+    INTEGER, PARAMETER :: direction_length = 1
+
+    CHARACTER(LEN=instruction_length), DIMENSION(file_length) :: instructions
+    CHARACTER(LEN=direction_length), DIMENSION(file_length) :: directions
+    INTEGER, DIMENSION(file_length) :: magnitudes
+
+    CHARACTER(LEN=*), PARAMETER :: file = "day_1_practice_input.txt"
+
+    OPEN(2, FILE=file, STATUS="OLD", ACTION="READ")
+        DO i=1, file_length
+            READ(2,*) instructions(i)
+        END DO
+    CLOSE(2)
+
+    PRINT*, instructions(3)
 
 END PROGRAM day_1
