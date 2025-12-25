db59a9d06314a6ccc5b84315e80c6a1ba4c91b3b
GemedetAdept <vivian.zitek@gmail.com>
2025-12-25 16:39:49 -0700

    Initialize type t_range

--- 2025/day_2/mod_range.f90
+++ 2025/day_2/mod_range.f90
@@ -1,4 +1,11 @@
 MODULE range
     IMPLICIT NONE
     PRIVATE
+    PUBLIC :: t_range
+
+    TYPE t_range
+        INTEGER :: range_start
+        INTEGER :: range_end
+        INTEGER, DIMENSION(:), ALLOCATABLE :: ranges
+    END TYPE t_range
 END MODULE range
