cc8e7b640f805e0b29e3c4a07ea1632774c9a25b
GemedetAdept <vivian.zitek@gmail.com>
2025-12-27 05:58:44 -0700

    Create get_string_count

--- 2025/day_2/day_2.f90
+++ 2025/day_2/day_2.f90
@@ -2,7 +2,7 @@
 
 PROGRAM day_2
     USE mod_range, ONLY: t_range, get_count, alloc_ids_count
-    USE mod_string_funcs, ONLY: get_max_string_len
+    USE mod_string_funcs, ONLY: get_max_string_len, get_string_count
     IMPLICIT NONE
 
     ! <-- SETUP --> 
@@ -11,12 +11,16 @@ PROGRAM day_2
     CHARACTER(:), ALLOCATABLE :: in_ranges
     CHARACTER(LEN=1) :: split
     INTEGER :: len_longest_range
+    INTEGER :: range_count
 
     in_ranges = "11-22,95-115,998-1012"
     split = ","
 
     len_longest_range = get_max_string_len(in_ranges, split)
-    WRITE(*,*) len_longest_range
+    WRITE(*,"(I0)") len_longest_range
+
+    range_count = get_string_count(in_ranges, split)
+    WRITE(*,"(I0)") range_count
 
 
 END PROGRAM day_2
--- 2025/day_2/mod_string_funcs.f90
+++ 2025/day_2/mod_string_funcs.f90
@@ -1,14 +1,15 @@
 MODULE mod_string_funcs
     IMPLICIT NONE
     PRIVATE
-    PUBLIC :: get_max_string_len
+    PUBLIC :: get_max_string_len, get_string_count
 
 CONTAINS
 
     FUNCTION get_max_string_len(in_strings, in_split_char) RESULT(out_longest_string)
         CHARACTER(:), ALLOCATABLE :: in_strings
-        INTEGER :: in_strings_len
         CHARACTER(LEN=1) :: in_split_char
+        INTEGER :: in_strings_len
+
         INTEGER :: string_start
         INTEGER :: current_string_len
         INTEGER :: out_longest_string
@@ -19,14 +20,14 @@ CONTAINS
         current_string_len = 1
         out_longest_string = 1
 
-        DO i=1, LEN(in_strings)
+        DO i=1, in_strings_len
             IF (in_strings(i:i) .EQ. in_split_char) THEN
                 current_string_len = i - string_start
                 string_start = i + 1
             END IF
 
-            IF (i .EQ. LEN(in_strings)) THEN
-                current_string_len = (LEN(in_strings) - string_start) + 1
+            IF (i .EQ. in_strings_len) THEN
+                current_string_len = (in_strings_len - string_start) + 1
             END IF
 
             IF (current_string_len .GT. out_longest_string) THEN
@@ -35,5 +36,28 @@ CONTAINS
         END DO
 
     END FUNCTION get_max_string_len
+
+    FUNCTION get_string_count(in_strings, in_split_char) RESULT(out_string_count)
+        CHARACTER(:), ALLOCATABLE :: in_strings
+        CHARACTER(LEN=1) :: in_split_char
+        INTEGER :: in_strings_len
+
+        INTEGER :: out_string_count
+        INTEGER :: i
+
+        in_strings_len = LEN(in_strings)
+        out_string_count = 0
+
+        DO i=1, in_strings_len
+            IF (in_strings(i:i) .EQ. in_split_char) THEN
+                out_string_count = out_string_count + 1
+            END IF
+
+            ! Account for the last string in the input
+            IF (i .EQ. in_strings_len) THEN
+                out_string_count = out_string_count + 1
+            END IF
+        END DO
+    END FUNCTION get_string_count
 
 END MODULE mod_string_funcs
