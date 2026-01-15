a46b03f89affd6dd6207c41416b9868ae5ee62fe
GemedetAdept <vivian.zitek@gmail.com>
2026-01-15 07:07:58 -0700

    Delete previous string functions to redo them with different logic

--- 2025/day_2/day_2.f90
+++ 2025/day_2/day_2.f90
@@ -2,7 +2,7 @@
 
 PROGRAM day_2
     USE mod_range, ONLY: t_range, get_count, alloc_ids_count
-    USE mod_string_funcs, ONLY: get_max_string_len, get_string_count, get_alloc_array, split_string
+    USE mod_string_funcs
     IMPLICIT NONE
 
     ! <-- SETUP --> 
@@ -10,27 +10,8 @@ PROGRAM day_2
 
     CHARACTER(:), ALLOCATABLE :: in_ranges
     CHARACTER(LEN=1) :: split
-    INTEGER :: len_longest_range
-    INTEGER :: range_count
-
-    CHARACTER(:), ALLOCATABLE :: split_ranges(:)
 
     in_ranges = "95-115,998-1012"
     split = ","
-
-    len_longest_range = get_max_string_len(in_ranges, split)
-    WRITE(*,"(I0)") len_longest_range
-
-    range_count = get_string_count(in_ranges, split)
-    WRITE(*,"(I0)") range_count
-
-    split_ranges = get_alloc_array(len_longest_range, range_count)
-    WRITE(*,"(I0)") SIZE(split_ranges)
-
-    split_ranges = split_string(in_ranges, split)
-
-    DO i=1, range_count
-        WRITE(*,*) split_ranges(i:i)
-    END DO
 
 END PROGRAM day_2
