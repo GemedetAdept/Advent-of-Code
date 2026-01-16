13b4cc8918b8abdf519a10a990b1067dd8d30c33
GemedetAdept <vivian.zitek@gmail.com>
2026-01-15 21:10:11 -0700

    Create subroutine to get the count of split characters

--- 2025/day_2/day_2.f90
+++ 2025/day_2/day_2.f90
@@ -2,16 +2,28 @@
 
 PROGRAM day_2
     USE mod_range, ONLY: t_range, get_count, alloc_ids_count
-    USE mod_string_funcs, ONLY: t_split_string
+    USE mod_string_funcs, ONLY: t_split_string, init_split_string, get_split_count
     IMPLICIT NONE
 
     ! <-- SETUP --> 
     INTEGER :: i
 
-    CHARACTER(:), ALLOCATABLE :: in_ranges
-    CHARACTER(LEN=1) :: split
+    CHARACTER(:), ALLOCATABLE :: input_string
+    INTEGER :: input_string_len
+    CHARACTER(LEN=1) :: split_char
+    TYPE(t_split_string), ALLOCATABLE :: new_split_string
 
-    in_ranges = "95-115,998-1012"
-    split = ","
+    input_string = "11-22,95-115,998-1012"
+    input_string_len = LEN(input_string)
+    split_char = ","
+
+    new_split_string = t_split_string(input_string, split_char)
+    call init_split_string(new_split_string)
+    call get_split_count(new_split_string)
+
+    WRITE(*,*) new_split_string % input_string
+    WRITE(*,*) new_split_string % split_char
+    WRITE(*,*) new_split_string % input_length
+    WRITE(*,*) SIZE(new_split_string % split_indices)
 
 END PROGRAM day_2
--- 2025/day_2/mod_string_funcs.f90
+++ 2025/day_2/mod_string_funcs.f90
@@ -1,21 +1,56 @@
 MODULE mod_string_funcs
     IMPLICIT NONE
     PRIVATE
-    PUBLIC :: t_split_string
+    PUBLIC :: t_split_string, init_split_string, get_split_count
 
     TYPE t_split_string
         CHARACTER(LEN=:), ALLOCATABLE :: input_string
         CHARACTER(LEN=1) :: split_char
+        INTEGER :: input_length = 0
 
         INTEGER, ALLOCATABLE :: split_indices(:)
-        INTEGER, ALLOCATABLE :: range_lens(:)
-        INTEGER :: range_count
-        INTEGER :: max_range_len
+        INTEGER, ALLOCATABLE :: range_len_arr(:)
+        INTEGER :: range_count = 0
+        INTEGER :: max_range_len = 0
 
         CHARACTER(LEN=:), ALLOCATABLE :: ranges(:)
     END TYPE t_split_string
 
 CONTAINS
 
+    SUBROUTINE init_split_string(in_t_split)
+        TYPE(t_split_string) :: in_t_split
+        INTEGER :: in_string_len
+
+        in_string_len = LEN(in_t_split % input_string)
+        in_t_split % input_length = in_string_len
+    END SUBROUTINE init_split_string
+
+    SUBROUTINE get_split_count(in_t_split)
+        INTEGER :: i, split_count
+
+        TYPE(t_split_string) :: in_t_split
+        CHARACTER(LEN=:), ALLOCATABLE :: in_string
+        CHARACTER(LEN=1) :: in_split_char
+        INTEGER :: in_string_len
+
+        in_string = in_t_split % input_string
+        in_split_char = in_t_split % split_char
+        in_string_len = in_t_split % input_length
+
+        split_count = 0
+        DO i=1, in_string_len
+            IF (in_string(i:i) .EQ. in_split_char) THEN
+                split_count = split_count + 1
+            END IF
+
+            IF (i .EQ. in_string_len) THEN
+                split_count = split_count + 1
+            END IF
+        END DO
+
+        ALLOCATE(in_t_split % split_indices(split_count))
+
+    END SUBROUTINE get_split_count
 
 END MODULE mod_string_funcs
