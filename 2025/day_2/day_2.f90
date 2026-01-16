a5ada75c32ff0268e9091f326faf7e715dbac7b0
GemedetAdept <vivian.zitek@gmail.com>
2026-01-16 07:12:30 -0700

    Create subroutine to get the range count of a split string type

--- 2025/day_2/day_2.f90
+++ 2025/day_2/day_2.f90
@@ -1,7 +1,7 @@
 ! Memento mori, ut memento vivere
 
 PROGRAM day_2
-    USE mod_string_funcs, ONLY: t_split_string, init_split_string, get_split_count
+    USE mod_string_funcs, ONLY: t_split_string, init_split_string, get_split_count, get_range_count
     USE mod_range, ONLY: t_range, get_bounding_ids, get_id_count, populate_ids
     IMPLICIT NONE
 
@@ -11,7 +11,7 @@ PROGRAM day_2
     CHARACTER(:), ALLOCATABLE :: input_string
     INTEGER :: input_string_len
     CHARACTER(LEN=1) :: split_char
-    TYPE(t_split_string), ALLOCATABLE :: new_split_string
+    TYPE(t_split_string), ALLOCATABLE :: new_split
 
     CHARACTER(:), ALLOCATABLE :: input_range
     INTEGER :: range_len
@@ -22,14 +22,17 @@ PROGRAM day_2
     input_string_len = LEN(input_string)
     split_char = ","
 
-    new_split_string = t_split_string(input_string, split_char)
-    CALL init_split_string(new_split_string)
-    CALL get_split_count(new_split_string)
+    new_split = t_split_string(input_string, split_char)
+    CALL init_split_string(new_split)
+    CALL get_split_count(new_split)
+    CALL get_range_count(new_split)
+    ! CALL populate_ranges(new_split)
 
-    ! WRITE(*,*) new_split_string % input_string
-    ! WRITE(*,*) new_split_string % split_char
-    ! WRITE(*,*) new_split_string % input_length
-    ! WRITE(*,*) SIZE(new_split_string % split_indices)
+    WRITE(*,*) new_split % input_string
+    WRITE(*,*) new_split % split_char
+    WRITE(*,*) new_split % input_length
+    WRITE(*,*) SIZE(new_split % split_indices)
+    WRITE(*,*) new_split % range_count
 
     input_range = "998-1012"
     range_split = "-"
--- 2025/day_2/mod_string_funcs.f90
+++ 2025/day_2/mod_string_funcs.f90
@@ -1,7 +1,7 @@
 MODULE mod_string_funcs
     IMPLICIT NONE
     PRIVATE
-    PUBLIC :: t_split_string, init_split_string, get_split_count
+    PUBLIC :: t_split_string, init_split_string, get_split_count, get_range_count
 
     TYPE t_split_string
         CHARACTER(LEN=:), ALLOCATABLE :: input_string
@@ -9,9 +9,8 @@ MODULE mod_string_funcs
         INTEGER :: input_length = 0
 
         INTEGER, ALLOCATABLE :: split_indices(:)
-        INTEGER, ALLOCATABLE :: range_len_arr(:)
+        INTEGER, ALLOCATABLE :: range_lens(:)
         INTEGER :: range_count = 0
-        INTEGER :: max_range_len = 0
 
         CHARACTER(LEN=:), ALLOCATABLE :: ranges(:)
     END TYPE t_split_string
@@ -51,5 +50,77 @@ CONTAINS
 
         ALLOCATE(in_t_split % split_indices(split_count))
     END SUBROUTINE get_split_count
+
+    SUBROUTINE get_range_count(in_t_split)
+
+        TYPE(t_split_string) :: in_t_split
+        INTEGER, ALLOCATABLE :: in_split_indices(:)
+        INTEGER :: in_range_count
+
+        in_split_indices = in_t_split % split_indices
+        in_range_count = SIZE(in_split_indices)
+        in_t_split % range_count = in_range_count
+
+    END SUBROUTINE get_range_count
+
+    ! SUBROUTINE populate_ranges(in_t_split)
+    !     INTEGER :: i
+
+    !     TYPE(t_split_string) :: in_t_split
+    !     CHARACTER(LEN=:), ALLOCATABLE :: in_string
+    !     CHARACTER(LEN=1) :: in_split_char
+    !     INTEGER :: in_string_len
+
+    !     INTEGER, ALLOCATABLE :: in_split_indices(:)
+    !     INTEGER, ALLOCATABLE :: in_range_lens(:)
+    !     CHARACTER(LEN=:), ALLOCATABLE :: in_ranges(:)
+
+    !     INTEGER :: index_start
+    !     INTEGER :: in_range_count
+    !     INTEGER :: current_range_len
+    !     CHARACTER(LEN=:), ALLOCATABLE :: substring_range
+
+    !     in_string = in_t_split % input_string
+    !     in_split_char = in_t_split % split_char
+    !     in_string_len = in_t_split % input_length
+    !     in_split_indices = in_t_split % split_indices
+    !     in_range_lens = in_t_split % range_lens
+
+    !     in_range_count = in_t_split % range_count
+
+    !     index_start = 1
+    !     current_range_len = 0
+    !     in_range_count = 0
+
+    !     DO i=1, in_string_len
+    !         IF (i .EQ. in_split_indices(index_start)) THEN
+    !             substring_range = in_string(index_start:i-1)
+
+    !             in_ranges(index_start) = substring_range
+
+    !             index_start = index_start + 1
+    !             in_range_count = in_range_count + 1
+                
+    !         ELSE IF (i .EQ. in_string_len) THEN
+    !             substring_range = in_string(index_start:i)
+
+    !             in_ranges(index_start) = substring_range
+                
+    !             in_range_count = in_range_count + 1
+    !         END IF
+    !     END DO
+
+    !     DO i=1, in_range_count
+    !         current_range_len = LEN(in_ranges(i:i))
+    !         in_range_lens(i) = current_range_len
+    !     END DO
+
+    !     ALLOCATE(CHARACTER(in_string_len) :: in_ranges(in_range_count))
+    !     ALLOCATE(in_range_lens(in_range_count))
+
+    !     in_t_split % ranges = in_ranges
+    !     in_t_split % range_count = in_range_count
+    !     in_t_split % range_lens = in_range_lens
+    ! END SUBROUTINE populate_ranges
 
 END MODULE mod_string_funcs
