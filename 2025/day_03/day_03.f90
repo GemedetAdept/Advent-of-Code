cf2c9dab7595fee2741460cdb4f0b55686172507
GemedetAdept <vivian.zitek@gmail.com>
2026-01-25 16:47:44 -0700

    Update from previous minor editing

--- 2025/day_03/day_03.f90
+++ 2025/day_03/day_03.f90
@@ -1,15 +1,15 @@
 ! Memento mori, ut memento vivere
 
 PROGRAM day_03
-    USE mod_fileio, ONLY: file_readto_oneline
     IMPLICIT NONE
 
     INTEGER :: i, unit
     
     CHARACTER(LEN=:), ALLOCATABLE :: filename
-    CHARACTER(LEN=:), ALLOCATABLE :: in_instruct
+    CHARACTER(LEN=:), ALLOCATABLE :: in_instructions
+
+    filename = "day_03_input.txt"
 
     OPEN(UNIT=1, FILE=filename, STATUS="OLD", ACTION="READ")
-
 
 END PROGRAM day_03
